4652a63468bff52e0dd94113769fe6d2baf1891b
LeonardoGottero <leitogottero@gmail.com>
2025-08-01 08:15:38 -0300

    Banners mejorados

--- app/Views/Hello.php
+++ app/Views/Hello.php
@@ -38,18 +38,30 @@
         <div class="bienvenido">
             <h1>Bienvenido, <?= session()->get('accountname') ?></h1>
             <p>¿Que vas a hacer hoy?</p>
-            <div class="banner" style="background-image: url('<?= base_url('Images/Users.jpg') ?>');">
-                <p>Visualiza una lista de los usuarios.</p>
-                <a href="<?= site_url('Users') ?>"><button class="custom-button">Usuarios</button></a>
-            </div>
-            <div class="banner" style="background-image: url('<?= base_url('Images/Devices.jpg') ?>');">
-                <p>Visualiza tus dispositivos.</p>
-                <a href="<?= site_url('Devices') ?>"><button class="custom-button">Dispositivos</button></a>
-            </div>
-            <div class="banner" style="background-image: url('<?= base_url('Images/Logs.jpg') ?>');">
-                <p>Visualiza los registros.</p>
-                <a href="<?= site_url('Logs') ?>"><button class="custom-button">Registros</button></a>
-            </div>
+            <a href="<?= site_url('Users') ?>">
+                <div class="banner" style="background-image: url('<?= base_url('Images/Users.jpg') ?>');">
+                    <div class="banner-text">
+                        <h2>Usuarios</h2>
+                        <p>Revisa una lista de los miembros.</p>
+                    </div>
+                </div>
+            </a>
+            <a href="<?= site_url('Devices') ?>">
+                <div class="banner" style="background-image: url('<?= base_url('Images/Devices.jpg') ?>');">
+                    <div class="banner-text">
+                        <h2>Dispositivos</h2>
+                        <p>Visualiza tus dispositivos.</p>
+                    </div>
+                </div>
+            </a>
+            <a href="<?= site_url('Logs') ?>">
+                <div class="banner" style="background-image: url('<?= base_url('Images/Logs.jpg') ?>');">
+                    <div class="banner-text">
+                        <h2>Registros</h2>
+                        <p>Enterate de lo que paso los ultimos días.</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
     <footer class="footer">
